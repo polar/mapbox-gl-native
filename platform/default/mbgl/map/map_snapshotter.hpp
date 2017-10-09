b0bf34f240d696dce27dcaf35a37ea274e296b1f
Ivo van Dongen <info@ivovandongen.nl>
2017-10-09 16:49:29 +0300

    [default] mapsnapshotter - add function to get screen coordinate from latlng for snapshot
    - Wraps the TransformState for the snapshot so that the snapshotter itself is free to be re-used or destroyed

--- platform/default/mbgl/map/map_snapshotter.hpp
+++ platform/default/mbgl/map/map_snapshotter.hpp
@@ -3,6 +3,7 @@
 #include <mbgl/util/image.hpp>
 #include <mbgl/util/thread.hpp>
 #include <mbgl/util/optional.hpp>
+#include <mbgl/util/geo.hpp>
 
 #include <exception>
 #include <memory>
@@ -49,7 +50,8 @@ public:
     void setRegion(const LatLngBounds&);
     LatLngBounds getRegion() const;
 
-    using Callback = std::function<void (std::exception_ptr, PremultipliedImage)>;
+    using PointForFn = std::function<ScreenCoordinate (const LatLng&)>;
+    using Callback = std::function<void (std::exception_ptr, PremultipliedImage, PointForFn)>;
     void snapshot(ActorRef<Callback>);
 
 private:
